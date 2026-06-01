51d98725928abc450f8a5df76b00e7ee9732452f
bounsongphommasen56-bit <bounsongphommasen56@gmail.com>
2026-06-01 21:32:30 +0800

    fix admin

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import App from "./App";
 import Admin from "./admin/Admin";
-
-import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/admin" element={<Admin />} />
-      </Routes>
-    </BrowserRouter>
+    <Admin />
   </React.StrictMode>
 );
